3f7ad6a0daec9d15c09eb32a1113f49e8663ee82
Giorgio Colliva <95485973+Gramas87@users.noreply.github.com>
2022-01-27 17:38:26 +0100

    fixed

--- M3-D4.js
+++ M3-D4.js
@@ -14,7 +14,7 @@ window.onload = () => {
                  <img class="card-img-top" src="${resp[i].img}" alt="Card image cap">
                  <div class="card-body">
                  <h5 class="card-title">${resp[i].title}</h5>
-                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                 <p class="card-text"></p>
                  </div>
                  <ul class="list-group list-group-flush">
                  <li class="list-group-item">${resp[i].category}</li>
@@ -22,7 +22,7 @@ window.onload = () => {
                  
                  </ul>
                  <div class="card-body">
-                 <button type="button" class="btn btn-primary" onclick = "addToCart()" >Add to cart</button>
+                 <button type="button" class="btn btn-primary" onclick = "addToCart(event)" >Add to cart</button>
                  <button type="button" class="btn btn-primary" onclick = "skip(event)" >Skip</button>
                  </div>
                  </div>
@@ -40,10 +40,11 @@ window.onload = () => {
 const skip = event => event.target.closest('.card').remove()
 
 
-const addToCart = () => {
+const addToCart = (e) => {
+     
      const cart = document.querySelector(".cart")
-     const newbook = document.createElement("li")                               //has to be something like this...
-     newbook.innerHTML = event.target.closest('.card-title'.innerHTML)
+     const newbook = document.createElement("li")                              
+     newbook.innerHTML =e.target.closest(".card").querySelector("h5").innerText
      cart.appendChild(newbook)
 
 }
